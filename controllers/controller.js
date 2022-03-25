80f4aea621c6bf6606e80e42bd132357dbeb953a
WibuSyariah <mspa43@gmail.com>
2022-03-25 08:49:51 +0700

    user

--- controllers/controller.js
+++ controllers/controller.js
@@ -66,7 +66,7 @@ class Controller {
     res.render("createProfileForm", { id });
   }
 
-  static createProfile(req, res) {
+  static createProfile(req, res) { 
     const { alias, biodata } = req.body;
     const UserId = req.query.id;
     const createdAt = new Date();
